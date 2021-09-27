95a88844b2fd78f8fa29dd3c2eb3652947854e4c
Luis gutierrez <luisguti_97@hotmail.com>
2021-09-27 08:57:04 -0500

    problema 2

--- Modproblema2.vb
+++ Modproblema2.vb
@@ -1,13 +1,27 @@
 ﻿Module Modproblema2
-    Dim edad(0), tip(0) As Integer
-    Public Sub Cargar_Matriz(ByVal edd As Integer, ByVal ti As Integer)
-        Dim ind1, indi2 As Integer
-        ind1 = edad.Length
-        ReDim edad(ind1 + 1)
-        edad(ind1 + 1) = edd
-        indi2 = tip.Length
-        ReDim tip(indi2 + 1)
-        tip(indi2 + 1) = ti
+    Dim edad(0), tipos(0) As Integer
+
+    Public Sub Cargar_Edad(ByVal edd As Integer)
+        Dim ind1 As Integer
+        ind1 = edad.Length()
+        If ind1 = 1 Then
+            edad(ind1 - 1) = edd
+            ReDim Preserve edad(ind1)
+        ElseIf ind1 > 1 Then
+            edad(ind1 - 1) = edd
+            ReDim Preserve edad(ind1)
+        End If
+    End Sub
+    Public Sub Cargar_tipos(ByRef ti As Integer)
+        Dim indi2 As Integer
+        indi2 = tipos.Length()
+        If indi2 = 1 Then
+            tipos(indi2 - 1) = ti
+            ReDim Preserve edad(indi2)
+        ElseIf indi2 > 1 Then
+            tipos(indi2 - 1) = ti
+            ReDim Preserve edad(indi2)
+        End If
     End Sub
     Public Function Total_clientes_mayores60() As Integer
         Dim i As Integer
@@ -25,7 +39,7 @@
         Dim i As Integer
         v = 0
         For i = 0 To edad.Length - 1
-            If edad(i) >= 25 Then
+            If edad(i) >= 25 And edad(i) < 60 Then
                 v += 1
             End If
         Next
@@ -56,8 +70,8 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tip.Length - 1
-            If tip(i) = 1 Then
+        For i = 0 To tipos.Length - 1
+            If tipos(i) = 1 Then
                 c += 1
             End If
             t += 1
@@ -70,8 +84,8 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tip.Length - 1
-            If tip(i) = 2 Then
+        For i = 0 To tipos.Length - 1
+            If tipos(i) = 2 Then
                 c += 1
             End If
             t += 1
@@ -84,8 +98,8 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tip.Length - 1
-            If tip(i) = 3 Then
+        For i = 0 To tipos.Length - 1
+            If tipos(i) = 3 Then
                 c += 1
             End If
             t += 1
