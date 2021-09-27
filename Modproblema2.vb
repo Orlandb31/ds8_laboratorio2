ec768f3f8cefcbab7555e01830a7e31af20d047b
Luis gutierrez <luisguti_97@hotmail.com>
2021-09-27 10:52:46 -0500

    cambios

--- Modproblema2.vb
+++ Modproblema2.vb
@@ -1,6 +1,6 @@
 ﻿Module Modproblema2
-    Dim edad(0), tipos(0) As Integer
-
+    Dim edad(0) As Integer
+    Dim tipos(0) As Integer
     Public Sub Cargar_Edad(ByVal edd As Integer)
         Dim ind1 As Integer
         ind1 = edad.Length()
@@ -27,7 +27,7 @@
         Dim i As Integer
         Dim v As Integer
         v = 0
-        For i = 0 To edad.Length - 1
+        For i = 0 To edad.Length() - 2
             If edad(i) >= 60 Then
                 v += 1
             End If
@@ -38,7 +38,7 @@
         Dim v As Integer
         Dim i As Integer
         v = 0
-        For i = 0 To edad.Length - 1
+        For i = 0 To edad.Length() - 2
             If edad(i) >= 25 And edad(i) < 60 Then
                 v += 1
             End If
@@ -49,7 +49,7 @@
         Dim i As Integer
         Dim v As Integer
         v = 0
-        For i = 0 To edad.Length - 1
+        For i = 0 To edad.Length() - 2
             If edad(i) < 25 Then
                 v += 1
             End If
@@ -60,7 +60,7 @@
         Dim i As Integer
         Dim v As Integer
         v = 0
-        For i = 0 To edad.Length - 1
+        For i = 0 To edad.Length() - 2
             v += 1
         Next
         Return v
@@ -70,13 +70,14 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tipos.Length - 1
+        For i = 0 To tipos.Length() - 1
             If tipos(i) = 1 Then
                 c += 1
             End If
             t += 1
         Next
         v = c * t / 100
+        v = v * 100
         Return v
     End Function
     Public Function Total_clientes_porcentajeT2() As Double
@@ -84,13 +85,14 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tipos.Length - 1
+        For i = 0 To tipos.Length() - 1
             If tipos(i) = 2 Then
                 c += 1
             End If
             t += 1
         Next
         v = c * t / 100
+        v = v * 100
         Return v
     End Function
     Public Function Total_clientes_porcentajeT3() As Double
@@ -98,13 +100,14 @@
         Dim v As Double
         c = 0
         t = 0
-        For i = 0 To tipos.Length - 1
+        For i = 0 To tipos.Length() - 1
             If tipos(i) = 3 Then
                 c += 1
             End If
             t += 1
         Next
         v = c * t / 100
+        v = v * 100
         Return v
     End Function
 End Module
